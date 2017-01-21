10e418e5bcd60df2b2fc3d00ca2f9ab6340532ce
Spotline <clement.duquesne@gmail.com>
2017-01-22 00:08:36 +0100

    Input manager, slots, bricks, and associated prefabs

---
+++ Assets/ModulatorBrick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Piafs
+{
+    public class ModulatorBrick : MonoBehaviour
+    {
+        private float dragTime = 0.1f;
+        private bool clicked = false;
+        public ModulatorSlot slot;
+        public Oscillator oscillator;
+        Collider2D col;
+
+        void Start()
+        {
+            col = GetComponent<Collider2D>();
+        }
+
+        public void OnMouseDown()
+        {
+            InputManager.Drag(this);
+            col.enabled = false;
+            slot.GrabOscillator();
+        }
+
+        public void Drop(ModulatorSlot _slot)
+        {
+            slot = _slot;
+            col.enabled = true;
+            SnapOn(slot);
+            slot.DropOscillator(oscillator);
+        }
+
+        void SnapOn(ModulatorSlot _slot)
+        {
+            Vector3 originPos = _slot.transform.position;
+            originPos.z = _slot.transform.position.z;
+            transform.position = originPos;
+        }
+    }
+}
+
