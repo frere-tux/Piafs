3598ce1df784c9c8cee9216981965be888067fc0
Spotline <clement.duquesne@gmail.com>
2017-01-21 15:32:14 +0100

    Toolkit for db <-> volt conversions

---
+++ Assets/Toolkit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Toolkit {
+
+	public static float VoltToDb(float volume, float referenceVolume)
+	{
+		return 20f * Mathf.Log10(volume / referenceVolume);
+	}
+
+	public static float DbToVolt(float decibels)
+	{
+		return Mathf.Pow(10f, decibels / 20f);
+	}
+}
